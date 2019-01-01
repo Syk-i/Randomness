b32964dcce1f6cc336936ceb9cd38298cdd507a5
Syk-i <34940095+Syk-i@users.noreply.github.com>
2019-01-01 12:57:25 +0300

    Added FloorTiles and Wall prefabs

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+    public GameObject[] tiles;
+    public GameObject wall;
+
+    public List<Vector3> createdTiles;
+
+    public int tileAmount;
+    public int tileSize; //space between each movement of the generator which will be 16 units
+
+	void Start () {
+        StartCoroutine(GenerateLevel());
+		
+	}
+
+    IEnumerator GenerateLevel() //used for debugging
+    {
+        yield return 0;
+    }	
+}
